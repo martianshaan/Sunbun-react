83f9b66eaeb42ed68d0427415325ae4a81328c59
martianshaan <shantanu.kale@viit.ac.in>
2023-08-19 16:52:58 +0530

    Update: createRoot

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { makeServer } from "./server";
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { makeServer } from './server';
 
 // Call make Server
 makeServer();
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
 );
