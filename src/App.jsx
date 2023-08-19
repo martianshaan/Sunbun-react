d38e15b32d241af7fcec6658b505ff05589044cc
martianshaan <shantanu.kale@viit.ac.in>
2023-08-19 16:53:47 +0530

    Update : filename

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import './custom.styles.css';
+import Navbar from './components/navbar/navbar';
+
+function App() {
+  return (
+    <div>
+      <h1>hhh</h1>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
